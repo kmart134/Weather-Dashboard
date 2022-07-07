00175fb10cefaa7718f9c9230807fa1b122e3ba9
kmart134 <ksmgps@hotmail.com>
2022-07-06 20:46:12 -0400

    added fetchWeather function

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,79 @@
 //SEARCH
 var searchHistory= [];
 var openWeatherUrl = 'https://api.openweathermap.org';
-// var apiKey = '5fac388ef92055f2e3608e994607821c';
+var apiKey = '5fac388ef92055f2e3608e994607821c';
 
 //html Element references
 var searchForm = document.querySelector("#search-form");
-var searchText = document.querySelector("#search-input").value;
+var searchText = document.querySelector("#search-input");
 var today = document.querySelector("#today");
 var forecastContainer = document.querySelector("#forecast");
 var history = document.querySelector("#history");
+var button = document.querySelector("#search-button");
 
-//grabbing the values from the search, to later use in functions
 
-function searchHandle (){
-   console.log(searchText);
-    //grab the search content
+
+
+//grab the values from the search, to later use in functions
+var searchHandle = function(event){
+    event.preventDefault();
+var search= searchText.value.trim()
+   console.log(search);
+   console.log("working");
+
+   fetchLocation(search);
     // fetchLocation(data we received from search)
 }
-searchHandle();
 
 
-//FUNCTIONALITY for fethcing location--activity 3 in unit 6
+
+//FUNCTIONALITY for fethcing location
 //call the location API
 
 function fetchLocation (search){
-    //get data pertaining to location- using an API call
-    //fetchWeather()
+  
+    var locationURL= ("http://api.openweathermap.org/geo/1.0/direct?q="+ search + "&limit=1&appid="+apiKey)
+    
 
+    fetch(locationURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+
+      var lat = data[0].lat;
+      console.log (lat);
+      var lon = data[0].lon;
+      console.log (lon);
+
+ 
+      
+    fetchWeather(lat, lon); 
+  
+      
+})
+    //get data pertaining to location- using an API call
 }
 
 //FUNCTIONALITY for  fetching weather
 
-function fetchWeather(location){
+function fetchWeather(lat,lon){
+    
+
+    var weatherURL= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+
+    fetch(weatherURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+
+      var temperature = data.main.temp;
+      console.log(temperature);
+    })
+
 //get data for the weather in location we searched using an API call
 
 }
@@ -42,3 +84,5 @@ function renderWeather (data){
 }
 
 //History Feature .. use local storage
+
+button.addEventListener('click', searchHandle);
